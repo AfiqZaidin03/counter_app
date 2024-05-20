3cd0718088560886f919d7065bbe591a895c4fec
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-20 12:59:35 +0800

    [2] Done for cubit
    Change stateful to stateless

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,16 +3,11 @@ import 'package:counter_app/inc_dec_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final counterCubit = BlocProvider.of<CounterCubit>(context);
@@ -20,7 +15,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
